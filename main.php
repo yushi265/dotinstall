3c28654fffba064ef30b9a35a4bc41b72f285a70
yushi265 <ae265.1108@gmail.com>
2020-12-19 16:38:21 +0900

    finish

--- main.php
+++ main.php
@@ -1,4 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
 <?php
+class Post {
+    // public $id;
+    // public $message;
+    // public $likes;
+
+    public function show() {
+        echo "$this->message ($this->likes)<br />";
+    }
+
+}
 
 $dsn = 'mysql:host=127.0.0.1;dbname=database1;charset=utf8';
 $db_user = 'root';
@@ -12,8 +31,9 @@ $driver_options = [
 try {
     $pdo = new PDO($dsn, $db_user, $db_pass, $driver_options);
 } catch (PDOException $e) {
+    $pdo->rollback();
     exit('データベース接続失敗。' . $e->getMessage());
-}
+} 
 
 $pdo->query("DROP TABLE IF EXISTS posts");
 
@@ -30,25 +50,75 @@ $pdo->query(
     "INSERT INTO posts (message, likes) VALUES
     ('thanks',12),
     ('thanks',4),
-    ('ありがとう',15)"
+    ('arigato',15)"
 
 );
 
-$n = 10;
+$pdo->beginTransaction();
+$pdo->query(
+    "UPDATE posts SET likes = likes + 1 WHERE id = 1"
+);
+$pdo->query(
+    "UPDATE posts SET likes = likes - 1 WHERE id = 2"
+);
+$pdo->commit();
 
-// $pdo->query("DELETE FROM posts WHERE likes < 10");
-$pdo->query("DELETE FROM posts WHERE likes < $n");
+// $label = '[Good!]';
+// $n = 10;
+
+// $pdo->query("DELETE FROM posts WHERE likes < $n");
+
+// $stmt = $pdo->prepare(
+//     "UPDATE
+//         posts
+//     SET
+//         message = CONCAT(:label, message)
+//     WHERE
+//         likes > :n "
+// );
+// $stmt->execute(['label'=>$label, 'n'=>$n]);
+// echo $stmt->rowCount() . ' records update<br />';
+
+// $search = 't%';
+
+// $stmt = $pdo->prepare(
+//     "SELECT * FROM posts WHERE message like :search"
+// );
+// $stmt->execute(['search' => $search]);
+
+// $message = 'Merci';
+// $likes = 8;
+// $stmt = $pdo->prepare(
+//     "INSERT INTO
+//         posts (message, likes)
+//     VALUES (:message, :likes)"
+// );
+// $stmt->bindParam('message', $message, PDO::PARAM_STR);
+// $stmt->bindParam('likes', $likes, PDO::PARAM_INT);
+// $stmt->execute();
+// echo 'ID: ' . $pdo->lastInsertId() . ' inserted<br />';
+
+// $message = 'Gracias';
+// $likes = 5;
+// $stmt->execute();
+// echo 'ID: ' . $pdo->lastInsertId() . ' inserted<br />';
+
+// $message = 'Danke';
+// $likes = 11;
+// $stmt->execute();
+// echo 'ID: ' . $pdo->lastInsertId() . ' inserted<br />';
 
 $stmt = $pdo->query("SELECT * FROM posts");
-$posts = $stmt->fetchall();
+$posts = $stmt->fetchall(PDO::FETCH_CLASS, 'Post');
 foreach($posts as $post) {
-    printf(
-        '%s (%d)' . PHP_EOL,
-        $post['message'],
-        $post['likes']
-    );
+    $post->show();
 }
 
 
 
 ?>
+
+    
+</body>
+</html>
+
